c9869178811e7317bef80d537430b73c599cc05a
Moritz Fliedner <moritz@stanfordalumni.org>
2016-07-09 16:09:51 -0700

    revert reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -14,7 +14,7 @@
 #
 
 class Reservation < ActiveRecord::Base
-  # validates :guest_id, :restaurant_id, :date, :time, :guest_count, presence: true
+  validates :guest_id, :restaurant_id, :date, :time, :guest_count, presence: true
   belongs_to :user,
     foreign_key: :guest_id,
     primary_key: :id,
